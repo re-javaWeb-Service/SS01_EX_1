0493c5fa912f3ea499a35f40755a0df172c7e91d
lochuynh22 <huynhlatienloc123@gmail.com>
2026-05-25 23:40:11 +0700

    init

--- src/main/java/com/re/bt1/ProductController.java
+++ src/main/java/com/re/bt1/ProductController.java
@@ -5,12 +5,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping({"/api/v1/"})
 public class ProductController {
     static class Product {
         private String id; private String name; private double price;
